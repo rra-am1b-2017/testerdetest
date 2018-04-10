001e8bb486d17829f59fdad70a441c4514388042
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-04-10 23:41:26 +0200

    test cordova

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular'; 
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private alertCtrl: AlertController;
+  private test: string;
 
+  constructor(public navCtrl: NavController, public alerCtrl: AlertController) {
+    this.alertCtrl = alerCtrl;
+    this.test ="";
+  }
+
+  doAlert(tekst:string ) {
+    var alert = this.alertCtrl.create({
+      title: 'Hallo Allemaal!' + tekst,
+      message: 'We gaan met zijn allen naar het Circus',
+      buttons: ['Dat is goed']
+    });
+    this.test = "De groeten van Arjan de Ruijter";
+    alert.present()
   }
 
 }
